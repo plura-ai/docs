afd68f2c798691048a7f0c8e4c39ccb9c9772185
ritik <ritikkharya05@gmail.com>
2025-01-02 23:10:31 +0530

    hydradtion error fixed

--- src/components/sidebar/custom-footer.tsx
+++ src/components/sidebar/custom-footer.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { useTheme } from "next-themes";
 import { Switch } from "../ui/switch";
+import { useEffect, useState } from "react";
 
 export default function CustomSideFooter() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleTheme = (checked: boolean) => {
     setTheme(checked ? "dark" : "light");
   };
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <div className="p-4 w-full">
