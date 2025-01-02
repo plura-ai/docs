fc690fecc7b7aff20934808c212a01c02110fa5e
ritik <ritikkharya05@gmail.com>
2025-01-02 23:37:55 +0530

    fix theme switcher

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,22 +1,16 @@
 "use client";
-
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { NavGuide } from "./nav-guide";
 import { sidebarConfig } from "@/config/sidebar.config";
 import { VersionSwitcher } from "./version-switcher";
 import DialogDemo from "../searchbar/Searchbar";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { ChevronUp, User2 } from "lucide-react";
 import ThemeToggler from "../theme/toggler";
-import CustomSideFooter from "./custom-footer";
+
 
 export function AppSidebar() {
   return (
@@ -31,8 +25,8 @@ export function AppSidebar() {
       <SidebarContent>
         <NavGuide items={sidebarConfig.guide} />
       </SidebarContent>
-     <SidebarFooter>
-      <CustomSideFooter/>
+     <SidebarFooter className="border-t ">
+      <ThemeToggler />
      </SidebarFooter>
     </Sidebar>
   );
